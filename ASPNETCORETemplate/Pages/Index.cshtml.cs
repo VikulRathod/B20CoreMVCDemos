dcfae1f312099528f3627c7058b29c21f44c7263
vikul rathod <vikul.rathod.net@gmail.com>
2023-04-13 10:19:25 +0530

    Add project files.

---
+++ ASPNETCORETemplate/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASPNETCORETemplate.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public string Name { get; set; }
+        public string Message { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            // write logic here for data
+            Name = "VHAASH TECHNOLOGIES";
+        }
+
+        public void OnPost(string username)
+        {
+            Message = $"Hello, {username}";
+        }
+    }
+}
---
+++ MVCCoreProject/Controllers/EmployeeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCCoreProject.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string GetMessage()
+        {
+            return "Hello, Good Morning!!";
+        }
+    }
+}
