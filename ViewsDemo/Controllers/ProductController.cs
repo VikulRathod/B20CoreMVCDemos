0e962a9367cd4767355370849027a2c6f90e61c6
vikul rathod <vikul.rathod.net@gmail.com>
2023-04-18 10:10:49 +0530

    All types of views, html helpers, tag helpers in mvc

---
+++ ViewsDemo/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ViewsDemo.Models;
+
+namespace ViewsDemo.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            // return View(); // Index.cshtml
+            return View("demo");
+        }
+
+        public ViewResult Details()
+        {
+            ProductModel product = new ProductModel() 
+            {
+            Name = "Casual Shirt",
+            Price = 599,
+            CategoryName = "Mens Wear"
+            };
+
+            return View(product);
+        }
+
+        public ViewResult Edit(int? id)
+        {
+            ProductModel product = new ProductModel()
+            {
+                Name = "Casual Shirt",
+                Price = 599,
+                CategoryName = "Mens Wear"
+            };
+
+            return View(product);
+        }
+    }
+}
