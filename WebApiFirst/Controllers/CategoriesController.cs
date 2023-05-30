189d3271cc10c27e6768bfc59b932a71ad29ff54
vikul rathod <vikul.rathod.net@gmail.com>
2023-05-30 15:27:49 +0530

    web api demo project added

---
+++ WebApiFirst/Controllers/CategoriesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiFirst.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private Dictionary<int, string> categories = 
+            new Dictionary<int, string>();
+
+        public CategoriesController() {
+            categories.Add(1, "Men");
+            categories.Add(2, "Kids");
+            categories.Add(3, "Women");
+        }
+
+        // domain/categories = GET
+        [HttpGet(Name = "GetAllCategories")]
+        public List<string> GetAllCategories()
+        {
+            return new List<string>() { "Men", "Kids", "Women"};
+        }
+
+        // domain/categories/1 = GET
+        [HttpGet("{id}", Name = "GetCategoryById")]
+        public string GetById(int? id)
+        {
+            return categories[id ?? 1];
+        }
+    }
+}
