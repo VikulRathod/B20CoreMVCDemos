dab61ddd9abd830657f5265fcb5dc1c0fd3480f8
vikul rathod <vikul.rathod.net@gmail.com>
2023-06-01 09:59:48 +0530

    CRUD using API methods

--- WebApiFirst/Controllers/CategoriesDBController.cs
+++ WebApiFirst/Controllers/CategoriesDBController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
 using WebApiFirst.Models;
 
 namespace WebApiFirst.Controllers
@@ -14,21 +16,114 @@ namespace WebApiFirst.Controllers
             _db = db;
         }
 
-        [HttpGet]
-        public IActionResult GetAllCategories()
+        //[HttpGet(Name = "GetAll")]
+        //public IActionResult GetAll()
+        //{
+        //    var categories = _db.Categories.ToList();
+        //    return Ok(categories);
+        //}
+
+        [HttpGet(Name = "GetAllAsync")]
+        public async Task<IActionResult> GetAllAsync()
         {
-            var categories = _db.Categories.ToList();
+            var categories = await _db.Categories.ToListAsync();
             return Ok(categories);
         }
 
+        [ProducesResponseType(StatusCodes.Status201Created,Type = typeof(Category))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(string))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(string))]
         [HttpPost]
-        public IActionResult Create(Category category)
+        public IActionResult Create(CategoryModel category)
         {
-            _db.Categories.Add(category);
-            _db.SaveChanges();
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    Category cat = new Category()
+                    {
+                        Name = category.Name,
+                        IsActive = category.IsActive
+                    };
 
-            return Ok(category);
-            // return Created("GetAllCategories", null);
+                    _db.Categories.Add(cat);
+                    _db.SaveChanges();
+
+                    return Created("Create", cat);
+                }
+                catch (Exception ex)
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError, 
+                        ex.Message);
+                }
+            }
+
+            return BadRequest("Please check input data");
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Category))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(string))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(string))]
+        [HttpPut]
+        public IActionResult Update(int id, CategoryModel category)
+        {
+            if(id != category.Id)
+                return BadRequest("Please correct category Id");
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    Category cat = new Category()
+                    {
+                        Id = category.Id,
+                        Name = category.Name,
+                        IsActive = category.IsActive
+                    };
+
+                    _db.Categories.Update(cat);
+                    _db.SaveChanges();
+
+                    return Ok(cat);
+                }
+                catch (Exception ex)
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError,
+                        ex.Message);
+                }
+            }
+
+            return BadRequest("Please check input data");
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Category))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(string))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(string))]
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            if (id <= 0)
+                return BadRequest("Please correct category Id");
+
+            Category cat = _db.Categories.Find(id);
+
+            if (cat != null)
+            {
+                try
+                {
+                    _db.Categories.Remove(cat);
+                    _db.SaveChanges();
+
+                    return Ok(cat);
+                }
+                catch (Exception ex)
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError,
+                        ex.Message);
+                }
+            }
+
+            return NotFound($"Category with id {id} does not exists.");
         }
     }
 }
---
+++ WebApiFirst/Models/CategoryModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiFirst.Models
+{
+    public class CategoryModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 5)]
+        public string Name { get; set; }
+
+        [Required]
+        public bool IsActive { get; set; }
+    }
+}
